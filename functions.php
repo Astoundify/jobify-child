ebc0ccf625e6b297e0c3ddb33bb4340f10ede6d7
Spencer Finnell <spencerfinnell@gmail.com>
2014-09-11 12:05:39 -0400

    Make sure the child themes CSS is always loaded.

--- functions.php
+++ functions.php
@@ -7,3 +7,8 @@
  * @since Jobify Child 1.0.0
  */
 
+
+function jobify_child_styles() {
+    wp_enqueue_style( 'jobify-child', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'jobify_child_styles', 20 );
